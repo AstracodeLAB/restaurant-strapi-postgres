b6877012badaf72b59d07b818229243bff5f8e70
Aidablaya <you@example.com>
2024-07-17 13:14:06 +0200

    added url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
      
       <BrowserRouter>
 				<Routes>
-					<Route path='/' element={ <Landing />} />
+					<Route path='/restaurant-strapi-postgres' element={ <Landing />} />
 				</Routes>
 			</BrowserRouter>
     </>
