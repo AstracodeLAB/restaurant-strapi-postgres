4d7b20b8302308b6c47039e914ffe156165ae0a2
Aidablaya <aidablaya@gmail.com>
2024-07-22 16:22:01 +0200

    change tailwind

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -25,7 +25,7 @@ function Landing() {
         <div className='flex-grow flex flex-col md:flex-row w-full'>
           <header className='flex flex-col w-full align-center justify-center items-center mt-3 '>
             <img className='w-1/3 rounded-full' src={Logo} alt="Logo del restaurante" />
-            <ul className="grid text-2xl grid-cols-2 md:gap-2 md:text-3xl text-center md:flex md:flex-col w-full">
+            <ul className="grid text-2xl grid-cols-2 md:leading-0 md:text-3xl text-center md:flex md:flex-col w-full ">
               <li className="relative w-full md:w-full">
                 <button className='py-1 md:py-4' onClick={() => openSection('letter')}>
                   Carta <FontAwesomeIcon icon={faChevronRight} size="xs" />
@@ -34,7 +34,7 @@ function Landing() {
                 {activeSection === 'letter' && <Letter onClose={closeSection} />}
               </li>
               <li className="relative w-full md:w-full">
-                <button className='py-1 md:py-4' onClick={() => openSection('reservation')}>
+                <button className='py-1 md:py-4 lg:py:1' onClick={() => openSection('reservation')}>
                   Reserva <FontAwesomeIcon icon={faChevronRight} size="xs" />
                 </button>
                 <hr className="absolute left-0 w-full border-gray-300" />
@@ -66,7 +66,7 @@ function Landing() {
           </section>
         </div>
       </div>
-      <footer className='fixed bottom-0 w-full bg-gray-800 text-white flex flex-col md:flex-row justify-center items-center py-2 md:py-4 z-50 text-xs md:text-base'>
+      <footer className='fixed bottom-0 w-full bg-gray-800 text-white flex flex-col md:flex-row justify-center items-center py-2 md:py-4 z-50 text-xs '>
         <ul>
           <li>Política de cookies</li>
           <li>Aviso de cookies</li>
