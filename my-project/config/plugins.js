1946db2ffafe7d869682d48da73ce96aa5a8f90c
Aidablaya <aidablaya@gmail.com>
2024-07-22 18:27:25 +0200

    added jswtr Secret in plugins.js

--- my-project/config/plugins.js
+++ my-project/config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = {
+   
+  
+    'users-permissions': {
+      config: {
+        jwtSecret: process.env.JWT_SECRET || 'hiz/GiKMQCNBaGgdTerc9Q==', // Reemplaza 'your-secret-key' con el secreto generado
+      },
+    },
+  };
