7120ff6644b0baf27aa577a1103d67cf280812b6
Aidablaya <aidablaya@gmail.com>
2024-07-23 10:43:51 +0200

    add folder dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant-strapi-postgres/'
+  base: '/restaurant-strapi-postgres/',
+  build: {
+    outDir: 'dist', // Esta es la configuración predeterminada
+    // otras configuraciones de build
+  },
 
 })
