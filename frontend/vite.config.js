7a806c597bd08faa291cc2cefc20143b44585518
Aidablaya <aidablaya@gmail.com>
2024-07-23 12:15:28 +0200

    change vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant-strapi-postgres/', // Asegúrate de que este valor coincida con el subdirectorio en el que está alojado tu sitio
+  base: '/restaurant-strapi-postgres/', // Asegúrate de que este sea el subdirectorio correcto
   build: {
     outDir: 'dist',
-  },
-  server: {
-    open: true,
   },
 })
