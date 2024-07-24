4c61739e929d1c49e2c9cab2b80dbc202ebeb932
Aidablaya <aidablaya@gmail.com>
2024-07-24 13:47:31 +0200

    prueba comando

--- frontend/src/components/contact.jsx
+++ frontend/src/components/contact.jsx
@@ -9,7 +9,7 @@ function Contact({ onClose }) {
   
 
   return (
-    <div className="fixed bottom-11 md:top-0 right-0 md:w-1/2 h-[60%] md:h-full flex items-center bg-gray-900 bg-opacity-50 z-50 justify-center">
+    <div className="fixed  bottom-11 md:top-0 right-0 md:w-1/2 h-[60%] md:h-full flex items-center bg-gray-900 bg-opacity-50 z-50 justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg h-[80%] md:h-[70%] mx-4 md:max-h-[75%] overflow-y-auto max-w-xl mb-6">
         <section className='flex justify-between item-center align-center'>
           <h2 className="flex text-2xl md:text-3xl">CONTACTO</h2>
