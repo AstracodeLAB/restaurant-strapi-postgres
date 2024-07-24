16dff249e5a015aab84839e34d829d5f8cf9a4f6
Aidablaya <aidablaya@gmail.com>
2024-07-24 16:30:25 +0200

    8

--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -2,7 +2,13 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173', 'https://restaurant-strapi-postgres.onrender.com'], // Agrega aquí tus dominios permitidos
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
