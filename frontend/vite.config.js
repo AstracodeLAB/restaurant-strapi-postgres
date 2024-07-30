ad7182742e227349264cf5adc0aabc322c68b2a9
Aidablaya <aidablaya@gmail.com>
2024-07-30 16:48:15 +0200

    6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-//import { resolve } from 'path';
-
-// Cargar variables de entorno desde los archivos secretos
-
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/restaurant-strapi-postgres/frontend/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
   server: {
-    host: '0.0.0.0', 
-    port: 3000, 
+    host: '0.0.0.0',
+    port: 3000,
   },
   preview: {
-    host: '0.0.0.0', // Asegúrate de que Vite preview escuche en todas las interfaces de red
-    port: 3000, // El puerto debe coincidir con el servidor
+    host: '0.0.0.0',
+    port: 3000,
   },
-  
 });
