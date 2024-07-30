bc972aabb291ac08e1dcde576fff5876ed5fe2e3
Aidablaya <aidablaya@gmail.com>
2024-07-30 16:58:18 +0200

    6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/restaurant-strapi-postgres/frontend/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
-  },
+  base: '/',
+  
   server: {
     host: '0.0.0.0',
     port: 3000,
