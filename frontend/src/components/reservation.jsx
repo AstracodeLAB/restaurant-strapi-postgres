4c7aaa08fc285807ff374f75bd30bc86588bc0e4
Aidablaya <aidablaya@gmail.com>
2024-08-20 13:07:41 +0200

    2

--- frontend/src/components/reservation.jsx
+++ frontend/src/components/reservation.jsx
@@ -5,7 +5,7 @@ function Reservation({ onClose }) {
     <div className="fixed bottom-11 md:top-0 right-0 md:w-1/2 h-[60%] md:h-full flex items-center bg-gray-900 bg-opacity-50 z-50 justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg h-[80%] md:h-[70%] mx-4 md:max-h-[75%] overflow-y-auto max-w-xl min-w-[75%] mb-6">
         <section className='flex justify-between '>
-          <h2 className="flex text-2xl md:text-3xl">RESERVAS</h2>
+          <h2 className="flex text-2xl md:text-3xl">RESERVAS </h2>
           <button className="my-2 py-0 md:py-2 px-2 md:px-4 bg-gray-800 text-white rounded hover:bg-gray-700 text-base md:text-xl" onClick={onClose}>X</button>
         </section>
         
